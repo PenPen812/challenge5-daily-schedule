6a284a1acb1b1bd2eff40e9a99bcb4d2017e004b
PenPen812 <penelope.c.leung@gmail.com>
2022-10-06 16:12:40 -0500

    deleted develope folder

---
+++ assets/JS/script.js
@@ -0,0 +1,13 @@
+// 1. CurrentDay
+var today = moment().format("MMMM Do YYYY");
+
+
+// 2. Time blocks for standard business hours show on the schedule
+
+// 3. Color indicates whether in the past, present, or future
+
+// 4. Enter an event function into a time block
+
+// 5. Data saved in local storage
+
+// 6. Saved events persist after refreshing the page
