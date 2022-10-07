694b984f4a3acd56d9c2f54e7ebec4b17258d82f
PenPen812 <penelope.c.leung@gmail.com>
2022-10-06 19:58:31 -0500

    modified css,js and html file

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,13 +1,39 @@
+
+
 // 1. CurrentDay
-var today = moment().format("MMMM Do YYYY");
+var currentDayEl = $('#currentDay');
 
+function displayTime() {
+    var rightNow = moment().format('dddd MMM DD, YYYY [at] h:mm:ss a');
+    currentDayEl.text(rightNow);
+}
 
+displayTime();
 // 2. Time blocks for standard business hours show on the schedule
-
+    hourEl.attr('class','hour')
+    timeBlockEl.attr('class','timeBlock')
+    descriptionEl.attr('class','description')
 // 3. Color indicates whether in the past, present, or future
-
+    pastEl.attr('class','past')
+    presentEl.attr('class','present')
+    futureEl.attr('class','future')
 // 4. Enter an event function into a time block
-
+    textareaEl.text('class','textarea')
 // 5. Data saved in local storage
+    saveBtnEl.attr('class','saveBtn')
+
 
 // 6. Saved events persist after refreshing the page
+
+
+  // set new submission to local storage 
+ 
+  var submission = $('description');
+
+  var description = function (event) {
+    event.preventDefault();
+  
+    var submission = descriptionEl.val();
+  
+    printDescription(submission);
+  }
